3183a8b90efed7b3e2b7cf2c4b32067ac276abcb
Crawford <907000795@qq.com>
2019-08-05 12:22:32 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import _ from 'lodash'
+import './style.css'
+import MyImg from './banner_1.png'
+import Data from './data.xml'
+
 function component() {
   const element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
+
+  //添加图片
+  var myImg = new Image()
+  myImg.src = MyImg
+
+  element.appendChild(myImg)
+
+  console.log(Data)
 
   return element
 }
