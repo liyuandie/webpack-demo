b673ab9c546fb865d0971a7a91d700876decf2ef
Crawford <907000795@qq.com>
2019-08-06 16:45:09 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import printMe from './print.js'
+import './style.css'
 
 function component() {
   const element = document.createElement('div')
@@ -15,4 +16,16 @@ function component() {
   return element
 }
 
-document.body.appendChild(component())
+// document.body.appendChild(component())
+
+let element = component() // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated pringtMe module!')
+    document.body.removeChild(element)
+    element = component() //重新渲染后，componet更新click事件处理
+    document.body.appendChild(element)
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    // app: './src/index.js',
+    // print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['dist'] }),
     new HtmlWebpackPlugin({
-      title: '管理输出'
-    })
+      title: 'Hot Module Replacement'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
   }
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       use: ['style-loader', 'css-loader']
-  //     },
-  //     {
-  //       test: /\.(png|svg|jpg|gif)$/,
-  //       use: ['file-loader']
-  //     },
-  //     {
-  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
-  //       use: ['file-loader']
-  //     },
-  //     {
-  //       test: /\.(csv|tsv)$/,
-  //       use: ['csv-loader']
-  //     },
-  //     {
-  //       test: /\.xml$/,
-  //       use: ['xml-loader']
-  //     }
-  //   ]
-  // }
 }
